aeb1c41bb3e5c2465e4265f33fd79e44672fcdd6
DaMingMing <1044096131@qq.com>
2019-07-01 01:15:49 +0800

    修改请求为action,更新小计

--- src/main/webapp/js/cart.js
+++ src/main/webapp/js/cart.js
@@ -3,6 +3,7 @@ var CART = {
 		$(".increment").click(function(){//＋
 			var _thisInput = $(this).siblings("input");
 			_thisInput.val(eval(_thisInput.val()) + 1);
+			//请求如果是html后缀且返回了Json对象。此时.html并不会解析该对象，会报错406。解决方法：请求改为.aciont后缀
 			$.post("/cart/update/num/"+_thisInput.attr("itemId")+"/"+_thisInput.val() + ".action",function(data){
 				CART.refreshTotalPrice();
 			});
@@ -13,6 +14,7 @@ var CART = {
 				return ;
 			}
 			_thisInput.val(eval(_thisInput.val()) - 1);
+			//请求如果是html后缀且返回了Json对象。此时.html并不会解析该对象，会报错406。解决方法：请求改为.aciont后缀
 			$.post("/cart/update/num/"+_thisInput.attr("itemId")+"/"+_thisInput.val() + ".action",function(data){
 				CART.refreshTotalPrice();
 			});
@@ -29,6 +31,14 @@ var CART = {
 		$(".itemnum").each(function(i,e){
 			var _this = $(e);
 			total += (eval(_this.attr("itemPrice")) * 10000 * eval(_this.val())) / 10000;
+			//更新小计
+			var a = _this.parent().parent().siblings(".pSubtotal").children(".totalprice").html();
+            _this.parent().parent().siblings(".pSubtotal").children(".totalprice").html(new Number((eval(_this.attr("itemPrice")) * 10000 * eval(_this.val())) / 10000/100).toFixed(2)).priceFormat({ //价格格式化插件
+                prefix: '¥',
+                thousandsSeparator: ',',
+                centsLimit: 2
+            });
+
 		});
 		$("#allMoney2").html(new Number(total/100).toFixed(2)).priceFormat({ //价格格式化插件
 			 prefix: '¥',
